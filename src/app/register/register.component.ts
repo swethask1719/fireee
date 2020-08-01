62ab6c12afd91d72a741177ac8f187c3dfa4d38a
swethask1719 <swetha.sasanapuri@gmail.com>
2020-08-01 12:02:34 +0530

    validate form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email: any;
+  password: any;
+  message="";
+  errorMessage= ""; //validation error handle
 
+  error: {name:string,message:string} = {name: '' , message: ''}; //for firebase error handle
   constructor() { }
 
   ngOnInit() {
   }
+  register(){
+
+  }
+  validateForm(email,password)
+  {
+    if(email.length === 0)
+    {
+      this.errorMessage = "please enter email id";
+      return false;
+    }
+    if(password.length === 0)
+    {
+      this.errorMessage = "please enter password";
+      return false;
+    }
+
+    if(password.length < 6)
+    {
+      this.errorMessage = "password should be atleast 6 characters";
+      return false;
+    }
+
+    this.errorMessage='';
+    return true;
+  }
+
 
 }
