e270982ad245e73b149232f4c72d60573110dcf4
swethask1719 <swetha.sasanapuri@gmail.com>
2020-08-05 07:08:53 +0530

    employee create with userid

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private fireService:AngularFirestore,private authService:AuthService) { }
+
+  create_Newemployee(Record)
+  { var owner=this.authService.authState;
+
+    return this.fireService.collection('Users/'+owner.uid+'/Employee').add(Record);
+  }
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud.service';
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
+  name: string;
+  age: number;
+  address: string;
+  message:string;
+  constructor(private crudService:CrudService) { }
 
   ngOnInit() {
+  }
+
+  create(){
+
+    let Record= {};
+
+    Record['name']=this.name;
+    Record['age']=this.age;
+    Record['Address']=this.address;
+
+    this.crudService.create_Newemployee(Record).then(res=>{
+      this.name="";
+        this.age=undefined;
+        this.address="";
+        alert("Employee created successfully")
+
+        console.log(res);
+    }).catch(error => {
+      console.log(error);
+  })
+
   }
 
 
