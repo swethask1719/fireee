7640de063b5bbbe18014ecd6baad8d43ded83b2b
swethask1719 <swetha.sasanapuri@gmail.com>
2020-08-05 07:23:11 +0530

    forgot password complete

--- src/app/forgetpassword/forgetpassword.component.ts
+++ src/app/forgetpassword/forgetpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-forgetpassword',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgetpassword.component.css']
 })
 export class ForgetpasswordComponent implements OnInit {
+  email: any;
 
-  constructor() { }
+  constructor(private af:AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  reset()
+  {
+    this.af.sendPasswordResetEmail(this.email).then(()=>{
+      alert('email sent');
+    }).catch((_error)=>{
+      console.log(_error);
+    })
   }
 
 }
